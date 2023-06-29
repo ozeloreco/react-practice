16fe574d04f358a16ae85a49c2e2a81eb0e35b65
ozeloreco <ozeloreco@gmail.com>
2023-06-29 12:01:32 +0100

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
 import React  from 'react';
-
+import { Link } from 'react-router-dom';
 export default function Home(){
     return<>
     <h1 className="text-center  m-5">React Projects</h1>
   <div className='projects d-flex flex-column align-items-center'>
-    <a href="/todo-app"className="project">Todo App</a>
-    <a href="/expense-tracker"className="project">Expense</a>
-    <a href="/fake-store"className="project">Fake Store</a>
-    <a href="/movie-website"className="project">Movie Website</a>
+    < Link to ="/todo-app"className="project">Todo App</Link>
+    < Link to ="/expense-tracker"className="project">Expense</Link>
+    < Link to ="/fake-store"className="project">Fake Store</Link>
+    < Link to ="/movie-website"className="project">Movie Website</Link>
   </div>
     </>
 }
--- src/pages/MovieWebsite.js
+++ src/pages/MovieWebsite.js
@@ -1,76 +1,58 @@
-    import React, {useState } from "react";
-import Swal from "sweetalert2";
-    export default function MovieWebsite() {
-        const [text, setText] =useState();
-        const [movies, setMovies] = useState ([]);
+import React, { useState } from "react";
+export default function MovieWebsite() {
+  const [text, setText] = useState();
+  const [movies, setMovies] = useState([]);
 
-
-        function getMoviesHandler(e){
-            e.preventDefault();
-            if (text.length<3){
-                Swal.fire('')
-            }
-            fetch('http://www.omdbapi.com/?apikey=28aa4fb3&s=' + text)
-            .then(res=> res.json())
-            .then (data => setMovies(data.Search))
-            .catch(error => console.log('Error is' + error))
-        }
-
-    
-
-    return (
-        <div style={{backgroundColor:'black', color:'white'}}> 
-        
-        <h1 className="js-flix">RECO FLIX</h1>
-        <form onSubmit={getMoviesHandler} style={{backgroundColor:'white', padding:'20px'}}>
-        <input onChange={e=> setText(e.target.value)} className="js-input-flix" placeholder="Search for your favourite Movie" type="text" />
-        <button className="js-button-flix">Search</button>
-        
-        </form>
-        <div className="bg-dark">
-            
-        </div>
-        
-
-        <div>
-        <h3 className="m-5 text-center">MOVIE GALLERY</h3>
-          <section className="d-flex flex-wrap justify-content-center px-1">
-           {movies.map(x=>{
-return  <div key={x.imdbID} className="card" style={{ width: "18rem" }}>
-<img
-  src={x.Poster}
-  className="card-img-top"
-  alt="..."
-/>
-<div className="card-body">
-  <h5 className="card-title">{x.Title}</h5>
-
-  <p className="card-text">{x.Year} {x.Type}
-
-  </p>
-  <a href="#" className="btn btn-danger w-100">see details
-  
-  </a>
-</div>
-</div>
-
-
-
-           })}
-           
-             
-            
-           
-            
-          </section>
-
-            
-        
-
-    
-        </div>
-        
-
-        </div>
-    );
+  function getMoviesHandler(e) {
+    e.preventDefault();
+    if (text.length < 3) {
+      // Swal.fire('Plese Insert Text');
     }
+    fetch("http://www.omdbapi.com/?apikey=28aa4fb3&s=" + text)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search))
+      .catch((error) => console.log("Error is" + error));
+  }
+
+  return (
+    <div style={{ backgroundColor: "black", color: "white" }}>
+      <h1 className="js-flix">RECO FLIX</h1>
+      <form
+        onSubmit={getMoviesHandler}
+        style={{ backgroundColor: "white", padding: "20px" }}
+      >
+        <input
+          onChange={(e) => setText(e.target.value)}
+          className="js-input-flix"
+          placeholder="Search for your favourite Movie"
+          type="text"
+        />
+        <button className="js-button-flix">Search</button>
+      </form>
+      <div className="bg-dark"></div>
+
+      <div>
+        <h3 className="m-5 text-center">MOVIE GALLERY</h3>
+        <section className="d-flex flex-wrap justify-content-center px-1">
+          {movies.map((x) => {
+            return (
+              <div key={x.imdbID} className="card" style={{ width: "18rem" }}>
+                <img src={x.Poster} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">{x.Title}</h5>
+
+                  <p className="card-text">
+                    {x.Year} {x.Type}
+                  </p>
+                  <a href="#" className="btn btn-danger w-100">
+                    see details
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    </div>
+  );
+}
